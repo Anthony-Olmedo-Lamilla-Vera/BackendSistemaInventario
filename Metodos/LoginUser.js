992fd145e6c8080d5fe51dd623c757d7c9d8a964
Lamilla19 <57071153+Lamilla19@users.noreply.github.com>
2021-11-01 13:29:59 -0500

    make it better

--- Metodos/LoginUser.js
+++ Metodos/LoginUser.js
@@ -5,28 +5,29 @@ const jwt = require("jsonwebtoken");
 const Login = async (req, res) => {
   const { Correo, Contraseña } = req.body;
   const LoginUser = await ModelUser.findOne({ Correo: Correo }).catch((err) =>
-    console.error("err")
+    res.status(400).json({
+      msg: "Este Correo No existe ",
+    })
   );
-  const ContraseñaHash = await bcrypt.compare(Contraseña, LoginUser.Contraseña);
-  try {
-    if (ContraseñaHash) {
-      const user = {
-        nombre: LoginUser.Nombre,
-        id: LoginUser._id,
-      };
-      const Token = jwt.sign(user, "Antox22", { expiresIn: "2h" });
-      res.setHeader("TokenUser", Token);
-      console.log(Token);
-      res.status(200).send({ token: Token, message: "Logeo Correcto" });
-    }
-    if (!ContraseñaHash) {
-      res.status(404).json({ msg: "Correo o Contraseña Incorrecta" });
-    }
-  } catch (err) {
-    res.status(400).send({
-      err: err,
-      msg: { 0: "Correo o Contraseña Incorrecta", 1: "Intenta Nuevamente " },
-    });
+  if (!LoginUser) {
+    res.status(400).send({ err: "Correo no existe" });
+  }
+  const ContraseñaHash = await bcrypt
+    .compare(Contraseña, LoginUser.Contraseña)
+    .catch((err) => res.status(400).json(err));
+
+  if (ContraseñaHash) {
+    const user = {
+      nombre: LoginUser.Nombre,
+      id: LoginUser._id,
+    };
+    const Token = jwt.sign(user, "Antox22", { expiresIn: "2h" });
+    res.setHeader("TokenUser", Token);
+    console.log(Token);
+    res.status(200).send({ token: Token, message: "Logeo Correcto" });
+  }
+  if (!ContraseñaHash) {
+    res.status(404).json({ msg: "Correo o Contraseña Incorrecta" });
   }
 };
 module.exports = Login;
--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-heroku ps:scale web=0
-web: bundle exec puma -t 5:5 -p 5000 -e development
+web : npm start
